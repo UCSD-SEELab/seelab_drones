99a43cc86d16862e470c7127090ea730bb91ff51
Stephen Wayne <swayne275@gmail.com>
2017-02-13 18:24:18 +0000

    Modified pre_mission.py

--- drone_scripts/pre_mission.py
+++ drone_scripts/pre_mission.py
@@ -20,8 +20,6 @@ with open('../database_files/mission_setup.json') as fp:
 
 db_name = 'mission_data'
 # username and password should be imported from models.py
-# local_user = 'root'
-# local_password = 'see2148'
 db_url = 'mysql+mysqldb://%s:%s@localhost/%s' %  (local_user, local_password, db_name)
 # db_url = 'mysql+mysqldb://root:password@localhost/' + db_name
 engine = create_engine(db_url)
