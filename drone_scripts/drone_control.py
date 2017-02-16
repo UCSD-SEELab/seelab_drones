477fb0549119e6d660b7ee4438f233bec6a70932
Stephen Wayne <swayne275@gmail.com>
2017-02-16 00:37:06 +0000

    Modified drone_control.py

--- drone_scripts/drone_control.py
+++ drone_scripts/drone_control.py
@@ -532,6 +532,8 @@ class Pilot(object):
             print " Waiting for vehicle {0} to initialise...".format(self.instance)
             # print(self.vehicle.gps_0.fix_type)
             time.sleep(1.0 / Pilot.sim_speedup)
+            if self.vehicle.gps_0.fix_type < 2:
+                print("Vehicle {0} waiting for GPS fix...".format(self.instance))
             ### TEST ONLY BELOW
             '''
             cnt += 1
